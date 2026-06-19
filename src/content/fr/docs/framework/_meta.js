b4e6a3451be318e1669352d14ffbc5bf4cf2f7d3
godfreyadero <gadero@aphrc.org>
2026-06-19 22:28:11 +0300

    Updates
    Changes made on 19062026 (Update on Bayesian, Translation and new images

---
+++ src/content/fr/docs/framework/_meta.js
@@ -0,0 +1,107 @@
+export default {
+  index: {
+    title: "Overview",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: false
+    }
+  },
+  "0-workflow": {
+    title: "Workflow Overview",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "1-analysis-setup": {
+    title: "Analysis Setup",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: false
+    }
+  },
+  "2-data-quality-assessment": {
+    title: "Data Quality Assessment",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "3-data-adjustment": {
+    title: "Data Adjustment",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "4-denominator-selection": {
+    title: "Denominator Selection",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "5-coverage-estimation": {
+    title: "Coverage Estimation",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "6-equity-analysis": {
+    title: "Equity Analysis",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "7-subnational-analysis": {
+    title: "Subnational Analysis",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "8-indicator-definitions": {
+    title: "Indicator Definition",
+    display: "hidden",
+    theme: {
+      layout: 'full',
+      copyPage: false,
+      toc: false
+    }
+  },
+    "9-mortality": {
+    title: "Mortality",
+    theme: { 
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  },
+  "10-service-utilisation": {
+    title: "Service Utilisation",
+    theme: { 
+      layout: 'full',
+      copyPage: true,
+      toc: true
+    }
+  },
+  "11-health-system-performance": {
+    title: "Health System Performance",
+    theme: { 
+      layout: 'full',
+      copyPage: false,
+      toc: true
+    }
+  }, 
+}
